c36d259fa1b84234035b27e507296ea5133cff56
fabiotrade <86625404+fabiotrade@users.noreply.github.com>
2022-01-16 05:21:59 -0300

    first commit

---
+++ src/exercicio2/cognizant/Main.java
@@ -0,0 +1,13 @@
+package exercicio2.cognizant;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Quadrláteros
+        System.out.println("Exercício Quadriláteros");
+        Quadrilateros.area(3);
+        Quadrilateros.area(5,5);
+        Quadrilateros.area(7,6,9);
+        Quadrilateros.area(5,5);
+    }
+}
